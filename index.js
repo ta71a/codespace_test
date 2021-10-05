e860388689eca7cd296dc69dbe0cfb36c768b1ed
ta71a <89366026+ta71a@users.noreply.github.com>
2021-10-05 09:38:24 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static('public'))
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index', {haikus: haikus});
+  res.render('index', {haikus: haikus, text: "aaa"});
 });
 
 app.listen(port);
